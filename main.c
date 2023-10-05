062e64e5cf82a2fa53c0284f0bdfee1a1a86a05d
jehkke <jbndoit@gmail.com>
2023-10-05 15:16:24 +0900

    실습4

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int year;
+	
+	printf("input a year:");
+	scanf("%d", &year);
+	
+	if ( (year % 4 ==0 && year % 100 !=0 ) || year % 400 == 0) /* () for first coding */
+		printf("Thid is leap year\n");
+	else printf("This in not leap year\n");
+	
+	
+	system("PAUSE");
+	return 0;
+}
