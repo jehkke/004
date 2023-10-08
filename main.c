784c6d05982e25f97b603590c6cca2e5b2ad8c19
jehkke <jbndoit@gmail.com>
2023-10-08 19:38:13 +0900

    실습6

--- main.c
+++ main.c
@@ -4,17 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+
+	int input;
+	int hour, min, sec;
 	
-	int year;
+	printf("input second:");
+	scanf("%d",&input);
 	
-	printf("input a year:");
-	scanf("%d", &year);
+	hour= input/3600;
+	min= (input%3600)/60;
+	sec= (input%3600)%60;
 	
-	if ( (year % 4 ==0 && year % 100 !=0 ) || year % 400 == 0) /* () for first coding */
-		printf("Thid is leap year\n");
-	else printf("This in not leap year\n");
+	
+	printf("The time is %d: %d: %d\n",hour, min, sec);
 	
 	
 	system("PAUSE");
 	return 0;
 }
+	
